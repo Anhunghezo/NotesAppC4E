88ca52287a4502e3e46002b8e6db8b0242739106
Nguyễn Quốc Đại <nguyendai1992@gmail.com>
2020-01-07 23:52:02 +0700

    update create note page

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let quill = new Quill('#function', {
+let quill = new Quill('#editor', {
     theme: 'snow'
-  });
- 
-  let Titles;
+});
+$('#save-success-alert button.close').on('click', () => {
+    $('#save-success-alert').removeClass('show');
+});
 
-document.getElementById("save").addEventListener("click",
-     function saveButton(){
+let titles;
+
+document.getElementById('save').addEventListener('click',
+    function saveButton(){
         let delta = quill.getContents(); 
         let converter = new QuillDeltaToHtmlConverter(delta.ops, {});
         let html = converter.convert(); 
-        console.log(html)
-        console.log(delta)
 
-      axios.post('https://5df38c84f9e7ae00148012bc.mockapi.io/NOTE4E/notes ', {
+        axios.post('https://5df38c84f9e7ae00148012bc.mockapi.io/NOTE4E/notes ', {
             delTa: delta.ops,
             Html: html,
-            title    : Titles,
-            Contents : 'IloveU',
-            
-       })
-       .then(function (response) {
-         console.log(response);
-         alert('Lưu thành công !');
-       })
-       .catch(function (error) {
-         console.log(error);
-       });
-      
-     })
+            title: titles,
+            Contents: 'IloveU',
+				
+        })
+            .then(function (response) {
+                $('#save-success-alert').addClass('show');
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+	
+    });
