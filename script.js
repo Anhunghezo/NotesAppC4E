12e5a9bedb3d16508db5e4c9d83871c5dfacc7e5
Huynhk <anhkieu303252@gmail.com>
2020-01-11 12:26:25 +0700

    change svae button From POST to PUT

--- script.js
+++ script.js
@@ -5,10 +5,33 @@ $('#save-success-alert button.close').on('click', () => {
     $('#save-success-alert').removeClass('show');
 });
 
+$('#update-success-alert button.close').on('click', () => {
+    $('#save-success-alert').removeClass('show');
+});
+
 let titles;
 
-document.getElementById('save').addEventListener('click',
-    function saveButton(){
+
+function getQuery () {
+    let urlParameter = window.location.search;
+    let praseUrl = urlParameter.slice(1).split('&');
+    let query = {};
+    praseUrl.forEach(item => {
+        let [key, value] = item.split('=');
+        query[key] = value;
+    });
+    return query;
+}
+
+    getQuery();
+    
+    let query = getQuery();
+    let noteId = query['note-id'];
+
+let buttondata = document.getElementById("save"); 
+
+if(!noteId){
+    buttondata.onclick = function saveButton(){
         let delta = quill.getContents(); 
         let converter = new QuillDeltaToHtmlConverter(delta.ops, {});
         let html = converter.convert(); 
@@ -28,4 +51,20 @@ document.getElementById('save').addEventListener('click',
                 console.log(error);
             });
 	
-    });
+    }
+}else{
+    buttondata.onclick = function updateButton(){
+        let delta = quill.getContents(); 
+        axios.put(`https://5df38c84f9e7ae00148012bc.mockapi.io/NOTE4E/notes/${noteId} `, {
+            delTa: delta.ops
+        })
+            .then(function (response) {
+                $('#update-success-alert').addClass('show');
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+	
+    }
+}
